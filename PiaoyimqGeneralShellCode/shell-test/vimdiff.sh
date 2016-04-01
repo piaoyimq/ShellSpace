7b6b19046d7ef05628566fa161b19c61350d29b5
Weibo Zhu <weibo.zhu@ericsson.com>
2016-04-01 12:34:34 +0200

    [shell-test]: Modify vimdiff.sh
    Change-Id: Iee3f303ffdf1be1d4e14b0059412b575d07748c1

--- PiaoyimqGeneralShellCode/shell-test/vimdiff.sh
+++ PiaoyimqGeneralShellCode/shell-test/vimdiff.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#Function: find a file in current directory compare with the $1 direcory file.
+#Usage: ./vimdiff.sh /var/log message
 #find . -type f -name $1 -print -exec vim {} \;
 file_list=$(find . -type f -name $2);
 array=($file_list)
@@ -12,12 +13,12 @@ then
 else
     if [ $length -eq 1 ]
     then
-        if [ -f "$base_dir$1/${array[0]:2}" ]
+        if [ -f "$1/${array[0]:2}" ]
         then
-            echo "vimdiff ${array[0]} $base_dir$1/${array[0]:2}"
-            vimdiff ${array[0]} $base_dir$1/${array[0]:2}
+            echo "vimdiff ${array[0]} $1/${array[0]:2}"
+            vimdiff ${array[0]} $1/${array[0]:2}
         else
-            echo "No this file in $base_dir$1/"
+            echo "No this file in $1/${array[0]:2}"
         fi
     else
         for ((i=0; i<$length; i++))
@@ -28,12 +29,12 @@ else
         read variable
         if expr $variable + 0 &>/dev/null && [ $variable -ge 0 ] && [ $variable -lt $length ]
         then
-            if [ -f "$base_dir$1/${array[$variable]:2}" ]
+            if [ -f "$1/${array[$variable]:2}" ]
             then
-                echo "vimdiff ${array[$variable]} $base_dir$1/${array[$variable]:2}"
-                vimdiff ${array[$variable]} $base_dir$1/${array[$variable]:2}
+                echo "vimdiff ${array[$variable]} $1/${array[$variable]:2}"
+                vimdiff ${array[$variable]} $1/${array[$variable]:2}
             else
-                echo "No this file in $base_dir$1/"
+                echo "No this file in $1/${array[$variable]:2}"
             fi
         else
             echo "No this file"
