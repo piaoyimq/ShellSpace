f751c65a5001644141aabeddb2da45ce9585c297
Weibo Zhu <weibo.zhu@ericsson.com>
2016-12-29 09:13:09 +0100

    modify expect scprit
    Change-Id: I550de2e5ded950ff666cbfdbf3179ddebbb9bba3

--- tools/shell-command/scp.sh
+++ tools/shell-command/scp.sh
@@ -2,11 +2,12 @@
 
 set ip [lindex $argv 0]
 
-spawn scp root@$ip:/md/loggd_persistent.log* .
+spawn scp erv@$ip:/md/loggd_persistent.log* .
 
 expect "*password:" 
 
-send "root\r"
-
+send "ggsn\r"
 expect eof 
+
+exec sh -c {gunzip *.gz}
 
