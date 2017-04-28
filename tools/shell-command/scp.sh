40433f4b5f1f3d4e44a5b0fa752625a00ad4e28d
Weibo Zhu <weibo.zhu@ericsson.com>
2017-04-28 11:12:52 +0200

    update scp.sh
    Change-Id: I4a50ffbee6b16d56d468ac424f3195749664348d

--- tools/shell-command/scp.sh
+++ tools/shell-command/scp.sh
@@ -1,24 +1,46 @@
-#! /usr/bin/expect -f 
+#!/bin/bash
+#set -x
 
-set ip [lindex $argv 0]
+PWD=`pwd`
+basedirname=`basename $PWD`
+dst_dir=`pwd`
+if [ "$basedirname" == $1 ]
+then
+    echo "parent is $1"
+else
+    echo "parent is not $1"
 
-spawn scp erv@$ip:/md/loggd_persistent.log* .
+    if [ ! -d $1 ]
+    then
+        mkdir $1
+    else
+        echo "current is $1"
+    fi
+    dst_dir=$dst_dir/$1
+    echo "new path: $dst_dir"
+fi
+
+copy()
+{
+/usr/bin/expect <<-EOF
+
+spawn scp erv@$1:/md/loggd_* $2
 
 expect "*password:" 
 
 send "ggsn\r"
-#expect eof 
+expect eof 
 
-#exec sh -c {gunzip *.gz}
 
-spawn scp erv@$ip:/md/services/epg/fm/alarm-history .
+spawn scp erv@$1:/md/services/epg/fm/alarm-history $2
 expect "*password:"
 send "ggsn\r"
 expect eof
 
-#exec sh -c {
-#if [ -f *.gz ]
-#then
-#    gunzip *.gz
-#fi
-#}
+EOF
+}
+
+
+copy $1 $dst_dir
+cd $dst_dir
+#exec ${SHELL-tcsh}
