e24ec2aa0d1b2fba233818b7596f6cefca8fd8ec
Weibo Zhu <weibo.zhu@ericsson.com>
2017-05-31 09:03:36 +0200

    Update git config
    Change-Id: Ib608d93b21076c656775af6c7a8562ee3b0fd4ea

--- env-setup.sh
+++ env-setup.sh
@@ -28,30 +28,13 @@ add_string_in_file()
 }
 
 ##############git config
-hist_command="[alias]
-    hist = log --graph --format=format:\\\"%C(red)%h%C(reset) %C(yellow)%ad%C(reset) | %s %C(green)\\\\[%an\\\\]%C(reset)%C(bold blue)%d%C(reset)\\\" --abbrev-commit --date=short"
-git_config=("hist" "test")
-length=${#git_config[@]}
-
-for ((i=0; i<$length; i++))
-do
-    case ${git_config[$i]} in
-           hist )
-            add_string_in_file "${git_config[$i]}" ".git/config" "$hist_command";;
-    
-           test )
-            #add_string_in_file "${git_config[$i]}" .git/config $hist_command;;
-              : #do nothing
-       esac
-done
- 
-git config --global color.status auto
-git config --global color.add auto
-#git config --global color.diff auto
-#git config --global user.name "piaoyimq"
-#git config --global user.email piaoyimq@qq.com
-
-############git mergetool vimdiff config:http://www.cnblogs.com/yaozhongxiao/p/3869862.html
+git_config()
+{
+   if [ ! -f ~/.gitconfig ] 
+   then
+       cp $BASE_PATH/ShellSpace/tools/git/.gitconfig ~/
+   fi
+}
 
 #shell
 
@@ -84,6 +67,7 @@ main ()
     export WS_ROOT
     echo "Initial workspace..."
 
+    git_config
     mk_symbolink
     PATH=$BASE_PATH/CppSpace/tools/bin:$BIN_PATH/compile:$PATH 
     export PATH=$(awk -F: '{for(i=1;i<=NF;i++){if(!($i in a)){a[$i];printf s$i;s=":"}}}'<<<$PATH) # Remove duplicates
