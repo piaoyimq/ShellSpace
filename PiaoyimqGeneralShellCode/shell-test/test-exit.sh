6e460837e4399adf30fa447eb9411a5641fea724
piaoyimq <piaoyimq@qq.com>
2017-06-02 12:06:36 +0200

    test-exit.sh
    Change-Id: Id38750a01ea2010842d2d04be09a5db3ea7979bb

---
+++ PiaoyimqGeneralShellCode/shell-test/test-exit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#set -e
+echo "1-1"
+cp
+echo "1-2"
+mkdir 
+echo "1-3"
+
+if [ -z "g" ]
+then
+    set -e
+    test
+    echo "1-4"
+else
+    set -e
+    
+    echo "1-5"
+fi
+
+    #set +e
+    cp
+    echo "1-6"
