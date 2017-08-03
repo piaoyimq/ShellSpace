c1b12055c72137cb36fd7f8fe9135cac23c8f6a4
Weibo Zhu <weibo.zhu@ericsson.com>
2017-08-03 07:55:01 +0200

    Update sed.sh
    Change-Id: I626bb40af1686b451da62a0e17913750a6510075

--- tools/shell-command/sed.sh
+++ tools/shell-command/sed.sh
@@ -8,4 +8,11 @@ fi
 
 
 set -x
+grep -w "$2" $1 -r
+set +x
+echo -en "\033[32mDo you want to replace?(y/n): \033[0m"
+read variable
+if [ $variable == 'y' ]
+then
 sed -i "s/$2/$3/g" `grep -w "$2" $1 -rl`
+fi
