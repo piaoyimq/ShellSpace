b6ca0d14b2af692c4860af18beebb1dd1899be55
Weibo Zhu <weibo.zhu@ericsson.com>
2017-11-27 15:57:14 +0800

    update ssh.sh
    Change-Id: I90ddac0022586fa4fed657fc10ca3c172940c989

--- tools/shell-command/ssh.sh
+++ tools/shell-command/ssh.sh
@@ -5,8 +5,9 @@ set ip [lindex $argv 0]
 spawn ssh erv@$ip
 
 expect {
- "*(yes/no)?" { send "yes\r" exp_continue}
- "*password:" {send "ggsn\r" }}
+ "*(yes/no)?" { send "yes\r"; exp_continue}
+ "*password:" {send "ggsn\r" }
+ }
 interact
 
 #exec ${SHELL-tcsh}
